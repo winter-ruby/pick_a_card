5c86be24c68e1da9cb00b8f0fb2b6223540d4f88
Ruby_Student <testrubymail.mail.ru>
2020-05-08 06:16:29 +0300

    version3

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 #make encoding in UTF:8
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ['♠', '♥', '♦', '♣']
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample} #{suits.sample}"
